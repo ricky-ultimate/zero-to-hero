9f7ca357ae09717067c63e32507ac1fc3b39e807
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-23 21:58:44 +0100

    feat: add some basic match commands, lets get going

--- habit_tracker/src/main.rs
+++ habit_tracker/src/main.rs
@@ -1,12 +1,27 @@
-use::std::io::{self, Write};
+use ::std::io::{self, Write};
 
 fn main() {
     println!("Welcome to habit tracker!");
-    print!("Type a command (add/list/quit): ");
-    io::stdout().flush().expect("Failed to flush stdout");
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).expect("failed to read commad!");
+    loop {
+        print!("Type a command (add/list/quit): ");
+        io::stdout().flush().expect("Failed to flush stdout");
 
-    println!("You typed: {}", command.trim());
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("failed to read commad!");
+
+        let command = command.trim();
+
+        match command {
+            "add" => println!("You chose to add a habit!"),
+            "list" => println!("You chose to list your habits"),
+            "quit" => {
+                println!("You chose to exit the program");
+                break;
+            }
+            _ => println!("Unidentified command, please try again."),
+        }
+    }
 }
