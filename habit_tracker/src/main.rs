b52f1b3f90e1314d12b74a00d544f3b234e4abab
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-23 23:17:06 +0100

    feat: add some substance to the code

--- habit_tracker/src/main.rs
+++ habit_tracker/src/main.rs
@@ -1,6 +1,12 @@
 use ::std::io::{self, Write};
 
+struct Habit {
+    name: String,
+    dates_done: Vec<String>,
+}
 fn main() {
+    let mut habits: Vec<Habit> = Vec::new();
+
     println!("Welcome to habit tracker!");
 
     loop {
@@ -15,8 +21,30 @@ fn main() {
         let command = command.trim();
 
         match command {
-            "add" => println!("You chose to add a habit!"),
-            "list" => println!("You chose to list your habits"),
+            "add" => {
+                print!("Enter habit name: ");
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                let mut name = String::new();
+                io::stdin()
+                    .read_line(&mut name)
+                    .expect("failed to read name");
+                let name = name.trim().to_string();
+
+                let new_habit = Habit {
+                    name,
+                    dates_done: Vec::new(),
+                };
+
+                habits.push(new_habit);
+                println!("Habit added!")
+            }
+            "list" => {
+                println!("Your habits: ");
+                for habit in &habits {
+                    println!(" - {} ", habit.name)
+                }
+            }
             "quit" => {
                 println!("You chose to exit the program");
                 break;
