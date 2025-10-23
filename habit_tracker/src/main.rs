f7db249e18c00ca6abffb1524852885771336f98
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-23 21:53:38 +0100

    feat: add some user input, easy stuff!

--- habit_tracker/src/main.rs
+++ habit_tracker/src/main.rs
@@ -1,3 +1,12 @@
+use::std::io::{self, Write};
+
 fn main() {
     println!("Welcome to habit tracker!");
+    print!("Type a command (add/list/quit): ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).expect("failed to read commad!");
+
+    println!("You typed: {}", command.trim());
 }
