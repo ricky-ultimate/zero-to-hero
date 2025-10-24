8fc7dd1dc7e503d81023405249342c6215f899fe
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-24 12:50:42 +0100

    feat: isolate stotage logic

---
+++ habit_tracker/src/storage.rs
@@ -0,0 +1,16 @@
+use std::fs;
+use crate::habit::Habit;
+
+
+fn load_habits()-> Vec<Habit> {
+    let data = fs::read_to_string("habits.json");
+    match data {
+        Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| Vec::new()),
+        Err(_) => Vec::new()
+    }
+}
+
+fn save_habits(habits: &Vec<Habit>) {
+    let data = serde_json::to_string_pretty(habits).expect("Failed to serialize habits");
+    fs::write("habits.json", data).expect("Failed to write to file")
+}
