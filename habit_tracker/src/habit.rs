1e003eb829ca749e21b885dc0416c36d5a3642aa
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-24 12:47:19 +0100

    feat: encapsulate habit logic

---
+++ habit_tracker/src/habit.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use chrono::Local;
+
+#[derive(Serialize, Deserialize)]
+pub struct Habit {
+    name: String,
+    dates_done: Vec<String>,
+}
+
+impl Habit {
+    pub fn new(name: String) -> Self {
+        Self {
+            name,
+            dates_done: Vec::new(),
+        }
+    }
+
+    pub fn mark_done(&mut self) {
+        let today = Local::now().format("%y-%m-%d").to_string();
+        self.dates_done.push(today.clone());
+        println!("Marked '{}' as done for '{}'", self.name, today)
+    }
+
+    pub fn summary(&self) {
+        println!("- {} ({} times done)", self.name, self.dates_done.len());
+    }
+}
