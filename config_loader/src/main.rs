0ee42a37b9334cae00e3e6626a6b877d6696052a
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-25 14:43:06 +0100

    chore: improve validation logic

--- config_loader/src/main.rs
+++ config_loader/src/main.rs
@@ -4,12 +4,15 @@ fn main() {
     let path = "test_data/.env";
 
     match config::read_file(path) {
-        Ok(content) => {
-            let config_map = config::parse_env(&content);
-            for (key, value) in config_map {
-                println!("{} : {}", key, value)
+        Ok(content) => match config::parse_env(&content) {
+            Ok(config_map) => {
+                for (key, value) in &config_map{
+                    println!("{} : {}", key, value)
+                }
             }
+
+            Err(e) => eprintln!("Parse error: {:?}", e),
         },
-        Err(e) => eprintln!("Error: {}", e),
+        Err(e) => eprintln!("Error: {:?}", e),
     }
 }
