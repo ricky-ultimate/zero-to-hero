72609e4f7d827046c4b926fc794bae1f95b0c2b4
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-25 08:55:15 +0100

    chore: test env parser function

--- config_loader/src/main.rs
+++ config_loader/src/main.rs
@@ -4,7 +4,12 @@ fn main() {
     let path = "test_data/.env";
 
     match config::read_file(path) {
-        Ok(content) => println!("File contents: \n{}", content),
+        Ok(content) => {
+            let config_map = config::parse_env(&content);
+            for (key, value) in config_map {
+                println!("{} : {}", key, value)
+            }
+        },
         Err(e) => eprintln!("Error: {}", e),
     }
 }
