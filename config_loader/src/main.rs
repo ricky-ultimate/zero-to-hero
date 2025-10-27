db8d77bbc10b6ebe78ac555e6c057a50b5db90a4
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-27 16:03:19 +0100

    chore: remove comments

--- config_loader/src/main.rs
+++ config_loader/src/main.rs
@@ -1,22 +1,5 @@
 mod config;
 
-// fn main() {
-//     let path = "test_data/.env";
-
-//     match config::read_file(path) {
-//         Ok(content) => match config::parse_env(&content) {
-//             Ok(config_map) => {
-//                 for (key, value) in &config_map{
-//                     println!("{} : {}", key, value)
-//                 }
-//             }
-
-//             Err(e) => eprintln!("Parse error: {:?}", e),
-//         },
-//         Err(e) => eprintln!("Error: {:?}", e),
-//     }
-// }
-
 fn main() {
     let config_path = "test_data/test.env";
 
