32bc8a83c7fb2b09eb0b7047dee78629042ee8f9
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-27 17:02:19 +0100

    feat: display results without option wrapping

--- config_loader/src/main.rs
+++ config_loader/src/main.rs
@@ -4,7 +4,21 @@ fn main() {
     let config_path = "test_data/test.env";
 
     match config::load_config(config_path) {
-        Ok(cfg) => println!("Loaded config: {:#?}", cfg),
+        Ok(cfg) => {
+            if let Some(port) = cfg.port {
+                println!("SERVER RUNNING ON PORT: {}", port);
+            }
+
+            if cfg.debug.unwrap_or(false) {
+                println!("Debug enabled");
+            }
+
+            if let Some(ref db) = cfg.database {
+                println!("Connecting to database at: {} ", db.url);
+            }
+
+            println!("Loaded config: {:#?}", cfg)
+        }
         Err(e) => eprintln!("Error loading config: {:?}", e),
     }
 }
