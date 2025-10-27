0e12ad6c41e94f8955fbfe6bc3922fce217effde
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-27 22:53:52 +0100

    chore: add minor configuration declaration

--- config_loader/src/services/mod.rs
+++ config_loader/src/services/mod.rs
@@ -1,6 +1,9 @@
 use crate::config::Config;
 
 pub fn start_app(config: &Config) {
+    println!("🚀 Starting app with the following configuration:");
+    println!("{:#?}", config);
+
     if let Some(port) = config.port {
         println!("Listening on port: {}", port);
     }
