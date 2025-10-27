8ce95ad6da7f24554db0a05aebe251af2d97b089
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-27 22:52:06 +0100

    chore: modularize app entry point

--- config_loader/src/main.rs
+++ config_loader/src/main.rs
@@ -1,23 +1,12 @@
 mod config;
+mod services;
 
 fn main() {
     let config_path = "test_data/test.env";
 
     match config::load_config(config_path) {
         Ok(cfg) => {
-            if let Some(port) = cfg.port {
-                println!("SERVER RUNNING ON PORT: {}", port);
-            }
-
-            if cfg.debug.unwrap_or(false) {
-                println!("Debug enabled");
-            }
-
-            if let Some(ref db) = cfg.database {
-                println!("Connecting to database at: {} ", db.url);
-            }
-
-            println!("Loaded config: {:#?}", cfg)
+            services::start_app(&cfg);
         }
         Err(e) => eprintln!("Error loading config: {:?}", e),
     }
