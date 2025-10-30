7f7351eef8d34cb7f483588d0e5fe5049c30cb35
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-30 15:22:10 +0100

    feat: add log in function

--- simple_auth/src/services/mod.rs
+++ simple_auth/src/services/mod.rs
@@ -1,7 +1,7 @@
 use crate::models::user::User;
 use crate::storage::{load_users, save_users};
 use argon2::password_hash::{SaltString, rand_core::OsRng};
-use argon2::{Argon2, PasswordHasher};
+use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 
 pub fn signup(username: &str, password: &str) {
     let mut users = load_users();
@@ -25,5 +25,26 @@ pub fn signup(username: &str, password: &str) {
         eprintln!("Failed to save user: {}", e);
     } else {
         println!("User registered successfully!")
+    }
+}
+
+pub fn login(username: &str, password: &str) {
+    let users = load_users();
+
+    match users.iter().find(|u| u.username == username) {
+        Some(user) => {
+            let parsed_hash = PasswordHash::new(&user.password_hash);
+            match parsed_hash {
+                Ok(hash) => {
+                    match Argon2::default().verify_password(password.as_bytes(), &hash) {
+                        Ok(_) => println!("Welcome, {}", username),
+                        Err(_) => println!("Invalid password"),
+                    }
+                },
+                Err(_) => println!("Invalid hash")
+            }
+        }
+
+        None => println!("User '{}' not found", username),
     }
 }
