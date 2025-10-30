2790aff766e0b6f2f6b3fee82a73f07c5c0fe525
Ricky Ultimate <rickyogigi@gmail.com>
2025-10-30 15:33:11 +0100

    feat: write main function

--- simple_auth/src/main.rs
+++ simple_auth/src/main.rs
@@ -1,7 +1,49 @@
 mod models;
-mod storage;
 mod services;
+mod storage;
+
+use services::{login, signup};
+use std::io::{self, Write};
 
 fn main() {
-    println!("Hello, world!");
+    println!("🪪 Simple Auth Service");
+
+    loop {
+        print!("Command (signup/login/quit): ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let cmd = input.trim();
+
+        match cmd {
+            "signup" => {
+                let (username, password) = get_input();
+                signup(username.trim(), password.trim());
+            }
+            "login" => {
+                let (username, password) = get_input();
+                login(username.trim(), password.trim());
+            }
+            "quit" => {
+                println!("👋 Exiting...");
+                break;
+            }
+            _ => println!("Unknown command."),
+        }
+    }
+}
+
+fn get_input() -> (String, String) {
+    print!("Enter username: ");
+    io::stdout().flush().unwrap();
+    let mut username = String::new();
+    io::stdin().read_line(&mut username).unwrap();
+
+    print!("Enter password: ");
+    io::stdout().flush().unwrap();
+    let mut password = String::new();
+    io::stdin().read_line(&mut password).unwrap();
+
+    (username, password)
 }
